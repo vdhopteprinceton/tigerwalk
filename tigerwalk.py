06c13856953b43d5c6cf47a6e7a9ed2fe2b3f94f
vdhopteprinceton <vdhopte@princeton.edu>
2021-01-26 15:21:46 -0500

    database setup and CAS

--- tigerwalk.py
+++ tigerwalk.py
@@ -1,27 +1,42 @@
 from flask import Flask
 from flask import render_template, make_response, request
 from flask_sqlalchemy import SQLAlchemy
+from CASClient import CASClient
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.secret_key = "tigerwalk key"
-# db = SQLAlchemy(app)
 
-# class shoeListing(db.Model):
-#     name = db.Column(db.String(150), nullable=False, primary_key=True)
-#     retail = db.Column(db.Integer, nullable=True)
-#     resale = db.Column(db.Integer, nullable=True)
-#     profit = db.Column(db.Integer, nullable=True)
-#     image = db.Column(db.Text, nullable=True)
-#     month = db.Column(db.String(10), nullable=False)
-#     date = db.Column(db.Integer, nullable=True)
+db = SQLAlchemy(app)
+class users(db.Model):
+    username = db.Column(db.String(150), nullable=False, primary_key=True) 
+    name = db.Column(db.String(150), nullable=False) 
+    phone = db.Column(db.Integer, nullable=True)
+    groupme = db.Column(db.String(150), nullable=True) 
+    facebook = db.Column(db.String(150), nullable=True) 
+    profilepic = db.Column(db.Text(), nullable=True) 
 
-#     def __repr__(self):
-#         return f"shoeListing('{self.name}', '{self.retail}', '{self.resale}', '{self.date}')"
+    def __repr__(self):
+        return f"shoeListing('{self.name}', '{self.phone}', '{self.groupme}', '{self.facebook}')"
 
 @app.route('/')
 def home():
+    username = CASClient().authenticate()
+    username = username.strip()
+    
     html = render_template('home.html')
+    response = make_response(html)
+    return response
+
+@app.route('/seewalkers')
+def activeWalkers():
+    html = render_template('activeWalkers.html')
+    response = make_response(html)
+    return response
+
+@app.route('/myprofile')
+def profile():
+    html = render_template('activeWalkers.html')
     response = make_response(html)
     return response
 
